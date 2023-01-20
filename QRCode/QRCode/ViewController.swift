0660341b4b198bf5cf485837f2566d95cc4108cc
Tomoya Asada <106445779+Asady1113@users.noreply.github.com>
2023-01-21 00:33:46 +0900

    Add files via upload

---
+++ QRCode/QRCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCode
+//
+//  Created by 浅田智哉 on 2023/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
